3754cece06c0ad979f6d8ac6ea4693cee9e1770a
Kamil <k.mirgasimov@innopolis.university>
2024-10-05 02:27:01 +0300

    api for calendar

---
+++ stubs/api/plants_calendar/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+
+const plants = [
+  {
+    name: "Rose",
+    deadline: "2024-10-09",
+    image: "https://ervanarium.com.br/wp-content/uploads/2019/03/cactus-3142589_1920.jpg",
+  },
+  {
+    name: "Sunflower",
+    deadline: "2024-10-05",
+    image: "https://avatars.mds.yandex.net/i?id=31da587c9aabc83ad3615023f91d7284781be06c-10701700-images-thumbs&n=13",
+  },
+];
+
+app.get('/api/plants', (req, res) => {
+  res.json(plants);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
